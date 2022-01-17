62753922144859773daca78dd7250ba622f370ed
JordiDarko <47432007+JordiDarko@users.noreply.github.com>
2022-01-17 12:10:36 +0100

    views correction

--- IOT/app1/urls.py
+++ IOT/app1/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from app1 import views
 
 urlpatterns = [
-    path('rele_on/', views.product_list),
-    path('rele_off/', views.order_list),
+    path('rele_on/', views.rele_on),
+    path('rele_off/', views.rele_off
+    ),
 ]
--- IOT/app1/views.py
+++ IOT/app1/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-import RPi.GPIO as GPIO
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+# import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(21, GPIO.OUT)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(21, GPIO.OUT)
 
+@api_view(('GET',))
 def rele_on(request):
     if request.method == 'GET':
-        GPIO.output(21, GPIO.HIGH)
+        # GPIO.output(21, GPIO.HIGH)
+        content = {'RELE': 'ON'}
+        return Response(content, status=status.HTTP_200_OK)
 
 def rele_off(request):
     if request.method == 'GET':
-        GPIO.output(21, GPIO.LOW)
+        # GPIO.output(21, GPIO.LOW)
+        content = {'RELE': 'OFF'}
+        return Response(content, status=status.HTTP_200_OK)
